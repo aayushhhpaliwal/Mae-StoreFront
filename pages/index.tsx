3ae72b6ecefa6a3f5038a30d2a58465826a9a0e4
aayushhhpaliwal <aayushpaliwal653@gmail.com>
2023-07-19 15:47:19 +0530

    Check the update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
 import { useEffect } from "react"
 import check from "@/maestore";
+import type { GetStaticProps, InferGetStaticPropsType } from "next"
 
+export async function getStaticProps() {
+    const products =[1,2,3]
+    return{
+      props : {
+        products
+      },
+      revalidate: 4*60*60
+    }
+}
 
+export default function Home( {
+  products
+}:InferGetStaticPropsType<typeof getStaticProps>) {
 
-export default function Home() {
-  const message: string = "Bhai Bhai"
-  useEffect (() => {
-    check();
-  }, [])
-  return (
-   <div>
-    {message}
-   </div>
+  return(
+    <div>
+      Hello World
+    </div>
   )
 }
+
