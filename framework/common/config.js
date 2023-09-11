d583a634f1653d26866b41e69812b5a836013c14
aayushhhpaliwal <aayushpaliwal653@gmail.com>
2023-09-11 10:20:11 +0530

    11 Sept

---
+++ framework/common/config.js
@@ -0,0 +1,15 @@
+const path = require ("path")
+const merge= require("deepmerge")
+const { defaultConfig } = require("next/dist/server/config-shared")
+
+function withFramworkConfig(defualtConfig={}){
+    const framework ="shopify"
+    "../shopify/next.config"
+    const frameworkNextConfig =  require(path.join("../", framework, "next.config"))
+    const config = merge(defaultConfig, frameworkNextConfig)
+
+    return config
+        
+    }
+    
+module.exports = {withFramworkConfig}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// const { withFramworkConfig } = require('./framework/common/config')
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+module.exports = {
+  i18n:{
+      locales:["en-US", "es"], 
+      defaultLocales:"en-US"
+  }
 }
 
-module.exports = nextConfig
+console.log("next.config.js", JSON.stringify(module.exports, null))
