41508333de828992b6c03febab0cf44b52e4b5ed
aayushhhpaliwal <aayushpaliwal653@gmail.com>
2023-09-22 15:02:36 +0530

    hellpo

--- framework/shopify/utils/queries/index.ts
+++ framework/shopify/utils/queries/index.ts
@@ -1,5 +1,3 @@
 
 
-
-
 export {default as getAllProductsQuery} from  "./get-all-produts"
--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-// const { withFramworkConfig } = require('./framework/common/config')
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
 
-module.exports = {
-  i18n:{
-      locales:["en-US", "es"], 
-      defaultLocales:"en-US"
-  }
-}
 
-console.log("next.config.js", JSON.stringify(module.exports, null))
